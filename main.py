5972e0b480b5314d56bfe8a7ab2f5d0d4028c360
lahcene <lmahidi@precise.earth>
2023-05-19 12:53:56 +0200

    create package dataset_extract_stock that will contain the all project to help getting datas from dataset files and put them on database. Inside we have a module called scrap_data.py that is the exact copy of addTittle.py that have been deleted a moment ago.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import addTitle as a
+from dataset_extract_stock import scrap_data as scrap
 
+# test scraping module
 imdb_id = "tt0111161"
-data_to_scrap = a.data_prop["title"]
-data = a.getData(imdb_id, data_to_scrap)
+data_to_scrap = scrap.data_prop["title"]
+data = scrap.getData(imdb_id, data_to_scrap)
 print(data)
 
 imdb_id = "tt0111161"
-data_to_scrap = a.data_prop["imdb_rating"]
-data = a.getData(imdb_id, data_to_scrap)
+data_to_scrap = scrap.data_prop["imdb_rating"]
+data = scrap.getData(imdb_id, data_to_scrap)
 print(data)
 
 imdb_id = "tt0111161"
-data_to_scrap = a.data_prop["release_year"]
-data = a.getData(imdb_id, data_to_scrap)
+data_to_scrap = scrap.data_prop["release_year"]
+data = scrap.getData(imdb_id, data_to_scrap)
 print(data)
