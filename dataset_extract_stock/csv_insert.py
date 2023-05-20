208cc17f24863840c65c4183d56685693e45ebbc
lahcene <lmahidi@precise.earth>
2023-05-20 13:47:54 +0200

    module aims to initiate a new csv with datas from source file pass to csvInit() function

---
+++ dataset_extract_stock/csv_insert.py
@@ -0,0 +1,17 @@
+
+
+
+# operation
+# Here we get all the lines from the file, for each line we add his title and append it in a list
+filelines = []
+"""with open(file_url, 'r') as f:
+    for line in f.readlines():
+        url_to_scrap = line[0:-13]
+        title = getTitle(url_to_scrap)
+        filelines.append(line.rstrip('\n') + "," + title + '\n')
+        print(title)
+
+# Here we just add in the precedent file each element from the list to a new line
+with open(file_url, 'w') as f:
+    for line in filelines:
+        f.write(line)"""
