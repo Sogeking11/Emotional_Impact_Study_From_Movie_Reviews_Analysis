fd16bd0ab0793c269bfa8ec7bd46bd4c5e32bbcb
lham72 <lham@hotmail.fr>
2023-06-07 13:56:11 +0200

    Avancé sur le modèle d'une table avec sqlalchemy

--- model/movie.py
+++ model/movie.py
@@ -1,2 +1,53 @@
-from sqlalchemy import create_engine
-engine = create_engine("mysql+mysqlconnector://user:password@host:port/CINE_EMOTION")
+from sqlalchemy import create_engine, Column, Integer, String, BLOB,LargeBinary, 
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+engine = create_engine("mysql+mysqlconnector://db_lm:131272@51.254.205.197:3306/CINEMOTION")
+
+
+"""
+metadata = db.MetaData() # extracting the metadata
+review = db.Table('movie', metadata, autoload=True, autoload_with=engine)
+
+print(repr(metadata.tables['movie']))
+"""
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+Base = declarative_base()
+
+
+class Movie(Base):
+    __tablename__ = 'movie'
+
+    id_movie = Column(Integer, primary_key=True)
+    id_imdb = Column(String(10))
+    title = Column(String(100))
+    production_company = Column(String(45))
+    pegi = Column(Integer)
+    synopsis = Column(BLOB)
+    keywords = Column(BLOB)
+    revenue = Column(Integer)
+    budget = Column(Integer)
+    review_score = Column(Integer)
+    release_date = Column(Integer)
+
+myBlob1 = BLOB("Il était une fois..")
+myBlob2 = BLOB("Maréchaux, villipendié, restroctirazion")
+
+movie1 = Movie(
+                    id_imdb='tt34535',
+                    title='Vorch',
+                    production_company='Warner Bross',
+                    pegi='18',
+                    synopsis=myBlob1,
+                    keywords=myBlob2,
+                    revenue=8,
+                    budget=75,
+                    review_score=5,
+                    release_date=2002
+                )
+
+session.add(movie1)
+session.commit()
