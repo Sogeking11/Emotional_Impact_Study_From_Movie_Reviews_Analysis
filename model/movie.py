278868e5e22be6215ea5add1e1ed01e383ed591f
lham72 <lham@hotmail.fr>
2023-06-09 16:49:15 +0200

    avancé sans réussite sur lien one to many

--- model/movie.py
+++ model/movie.py
@@ -1,15 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String, BLOB
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, BLOB, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine("mysql+mysqlconnector://db_lm:131272@51.254.205.197:3306/CINEMOTION")
 
-
-"""
-metadata = db.MetaData() # extracting the metadata
-review = db.Table('movie', metadata, autoload=True, autoload_with=engine)
-
-print(repr(metadata.tables['movie']))
-"""
 
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -32,9 +25,22 @@ class Movie(Base):
     budget = Column(Integer)
     review_score = Column(Integer)
     release_date = Column(Integer)
+    reviews = relationship("Review", back_populates="movie")
+
+
+class Review(Base):
+    __tablename__ = 'review'
+
+    id_review = Column(Integer, primary_key=True)
+    movie_id_movie = Column(Integer, ForeignKey('movie.id_movie'))
+    text = Column(BLOB)
+    movie = relationship("Movie", back_populates="review")
+
+Movie.review = relationship("Review", order_by = Review.id_review, back_populates = "movie")
 
 myBlob1 = b'Il etait une fois..'
 myBlob2 = b"Marechaux, villipendie, restroctirazion"
+
 
 movie1 = Movie(
                     id_imdb='tt34535',
@@ -49,5 +55,11 @@ movie1 = Movie(
                     release_date=2002
                 )
 
+review1 = Review(text=b"Hello de Lu", movie=movie1)
+review2 = Review(text=b"congratulation...", movie=movie1)
+
+
 session.add(movie1)
+session.add(review1)
+session.add(review2)
 session.commit()
