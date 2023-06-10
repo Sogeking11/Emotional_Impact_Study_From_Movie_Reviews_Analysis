a92f691399226502124d5752cec1e0726374a769
lahcene <lmahidi@precise.earth>
2023-06-10 14:14:34 +0200

    a step has been done, we can map 2 tables with one->many relation db

--- model/sqlalchemyconfig.py
+++ model/sqlalchemyconfig.py
@@ -2,9 +2,18 @@ from sqlalchemy import create_engine, Column, Integer, String, BLOB, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
+import dataset_extract_stock.mySecrets as s
 
 
-engine = create_engine("mysql+mysqlconnector://db_lm:131272@51.254.205.197:3306/CINEMOTION")
+
+engine = create_engine("mysql+mysqlconnector://" + s.DATABASE_USER
+                                                 + ":" + s.DATABASE_PASSWORD
+                                                 + "@" + s.DATABASE_HOST
+                                                 + ":" + s.DATABASE_PORT + "/"
+                                                 + "CINEMOTION"
+                                                 )
+
+
 
 Base = declarative_base()
 
