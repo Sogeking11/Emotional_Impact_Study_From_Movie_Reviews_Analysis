72ed76a9657c6725fa79841daff480e49472f068
lahcene <lmahidi@precise.earth>
2023-06-10 10:02:27 +0200

    without re-creating BDD's working, next step project structuration to separate each model table in a file.

--- model/movie.py
+++ model/movie.py
@@ -42,24 +42,24 @@ Base.metadata.create_all(engine)
 #Movie.reviews = relationship("Review", order_by = Review.id_review, back_populates = "movie")
 
 myBlob1 = b'Il etait une fois..'
-myBlob2 = b"Marechaux, villipendie, restroctirazion"
+myBlob2 = b"C'est alors qu'il voulurent, comme d'un pacte..."
 
 
 movie = Movie(
-                    id_imdb='tt34535',
-                    title='Vorch',
-                    production_company='Warner Bross',
-                    pegi='18',
+                    id_imdb='tt88888',
+                    title='V for Vendetta',
+                    production_company='Earth wind and Fire',
+                    pegi='30',
                     sysnopsis=myBlob1,
                     keywords=myBlob2,
-                    revenue=8,
-                    budget=75,
-                    review_score=5,
-                    release_date=2002
+                    revenue=4,
+                    budget=750000,
+                    review_score=8,
+                    release_date=2022
                 )
 
-review1 = Review(text=b"Hello de Lu", movie=movie)
-review2 = Review(text=b"congratulation...", movie=movie)
+review1 = Review(text=b"Exceptionnelle", movie=movie)
+review2 = Review(text=b"Pas terrible...", movie=movie)
 
 
 session.add(movie)
