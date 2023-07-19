892921240321a239208d8099af33be691f3a406f
redagrd <reda.guerrouad@gmail.com>
2023-07-19 14:08:26 +0200

    final scripts for first data fetching

--- ETL/IMDB_reviews_scrap.py
+++ ETL/IMDB_reviews_scrap.py
@@ -2,14 +2,18 @@ import requests
 from selenium import webdriver
 import time
 import pandas as pd
-import csv
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from datetime import datetime
+import json
+import api_keys
+from bs4 import BeautifulSoup
+
 
 # API TMDB
-api_key = '5849fbb34bea488bb2d73d4d6638ff36'
+api_key = api_keys.TMDB
+api_df = pd.read_json('C:\Users\Administrateur\Documents\Projet_final\data\data_full.json')
 
 def get_the_most_popular_movies_id(api_key):
     """_summary_ : This function returns the list of the most popular movies id from the TMDB API
@@ -61,21 +65,33 @@ def extract_movie_data_from_imdb_id(id_list):
     df = pd.DataFrame.from_dict(movie_data_list)
     return df
 
-def click_load_more(element):
-    '''_summary_ : This function clicks on the "See more" button to extend the full comment
-    '''
-    try:
-        see_more = element.find_element(By.CLASS_NAME, "ipl-expander")
-        if see_more.is_displayed():
-            see_more.click() 
-    except:
-        pass
+# def click_see_all(element):
+#     '''_summary_ : This function clicks on the "See more" button to extend the full comment
+#     '''
+#     try:
+#         see_all = element.find_element(By.CLASS_NAME, "ipl-expander")
+#         if see_all.is_displayed():
+#             see_all.click() 
+#     except:
+#         pass
+
+# def click_load_more():
+#     '''_summary_ : This function clicks on the "Load more" button to extend the full comment
+#     '''
+#     while True:
+#         try:
+#             load_more = driver.find_element(By.CLASS_NAME, "ipl-load-more__button")
+#             if load_more.is_displayed():
+#                 load_more.click() 
+#                 time.sleep(1)
+#         except:
+#             break
 
 # get the most popular movies id
 
-movies_ids = get_the_most_popular_movies_id(api_key)
-df = extract_movie_data_from_imdb_id([movies_ids[0]])
-id = df['id_movie']
+# movies_ids = get_the_most_popular_movies_id(api_key)
+df = api_df
+id = df['id_tmdb']
 
 url = f'https://api.themoviedb.org/3/movie/{id}'
 params = {'api_key': api_key}
@@ -83,83 +99,116 @@ response = requests.get(url, params=params)
 data = response.json()
 imdb_id = df['id_imdb']
 
-# Selenium configuration
-driver = webdriver.Chrome()  
-driver.maximize_window()
-wait = WebDriverWait(driver, 10)
+# # Selenium configuration
+# driver = webdriver.Chrome()  
+# driver.maximize_window()
+# wait = WebDriverWait(driver, 10)
 
 data_comments = []
 # get the reviews of the most popular movies
-while True:
-    for id in imdb_id:
-        url = f'https://www.imdb.com/title/{id}/reviews?ref_=tt_ov_rt'
-        driver.get(url)
-        time.sleep(1)
+def get_reviews():
+    """_summary_ : This function extracts the reviews from the IMDB website.
+    Returns:
+        data_comments: List of the reviews
+    """
+    # get only the first 30 reviews per movie
+    while True:
+        for id in imdb_id:
+            url = f'https://www.imdb.com/title/{id}/reviews?ref_=tt_ov_rt'
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            reviews = soup.find_all('div', class_='review-container')
+            
+            # # click "Load more" button until we get to 30 reviews
+            # if len(reviews) < 30:
+            #     try:
+            #         load_more_button = driver.find_element(By.CLASS_NAME, "ipl-load-more__button")
+            #         load_more_button.click()
+            #         time.sleep(1)
+            #     except:
+            #         break
+            
+            for review in reviews:
+                #click_see_all(review)
+                # get the title of the movie
+                try:
+                    title = df.loc[df['id_imdb'] == id, 'title'].iloc[0]
+                except:
+                    title = 'N/A'
+                    
+                # get the id of the movie
+                try:
+                    id = df.loc[df['id_imdb'] == id, 'id_imdb'].iloc[0]
+                except:
+                    id = 'N/A'
+                # get the review score
+                try:
+                    review_score = review.select_one('.rating-other-user-rating span').text
 
-        reviews = driver.find_elements(By.CLASS_NAME, "review-container")
-        for review in reviews[:5]:
-            click_load_more(review)
-            # get the title of the movie
-            try:
-                title = df.loc[df['id_imdb'] == id, 'title'].iloc[0]
-            except:
-                title = 'title not found'
-                
-            # get the id of the movie
-            try:
-                id = df.loc[df['id_imdb'] == id, 'id_imdb'].iloc[0]
-            except:
-                id = 'id not found'
-            # get the review score
-            try:
-                for score in review.find_elements(By.CLASS_NAME, "rating-other-user-rating"):
-                    review_score = score.text.replace('/10','')
-            except:
-                review_score = 'Score not found'
-            # get the review url and the user url
-            try:
-                lnks = review.find_elements(By.TAG_NAME,"a")
-                review_url = lnks[0].get_attribute('href')
-                user_url = lnks[1].get_attribute('href')
-            except:
-                review_url = 'URL not found'
-            # get the review date and convert it to the right format
-            try:
-                review_date = review.find_element(By.CLASS_NAME, "review-date").text
-                review_date = datetime.strptime(review_date, '%d %B %Y').strftime('%Y-%m-%d')
-            except:
-                review_date = 'date not found'
-            # get the username
-            try:
-                username = review.find_element(By.CLASS_NAME, "display-name-link").text
-            except:
-                username = 'user not found'
-            # get the comment of the user and remove the useless lines
-            try:
-                comment = review.find_element(By.CLASS_NAME, "content").text.replace('\n\n','\n')
-                comment = comment.split('\n')
-                comment = [line for line in comment if not 'Was this review helpful? Sign in to vote' in line] 
-                comment = [line for line in comment if not 'Permalink' in line]
-                comment = '\n'.join(comment)
-            except:
-                comment = 'comment not found'
-            # store the data in a list of dictionaries
-            data_comments.append({'id': id, 'title': title, 'user': username, 'comment': comment, 'score': review_score, 'user_url': user_url, 'review_url': review_url, 'date': review_date})
-    break
-
-csv_path = 'testV3.csv'
-
-# write the data in a csv file
-with open(csv_path, mode='w', newline='', encoding='utf-8') as csv_file:
-    fieldnames = ['id', 'title', 'user', 'comment', 'score', 'user_url', 'review_url', 'date']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(data_comments)
-
-print("Data saved in CSV file")
-
-# close the driver
-driver.quit()
+                except:
+                    review_score = '0'
+                # get the review url and the user url
+                try:
+                    lnks = review.find_all('a', href=True)
+                    review_url = lnks[0]['href']
+                    user_url = lnks[1]['href']
+                    # review_url = review.find('a', class_='title').get('href')
+                    # user_url = review.find('a', class_='display-name-link').get('href')
+                    # lnks = review.find_elements(By.TAG_NAME,"a")
+                    # review_url = lnks[0].get_attribute('href')
+                    # user_url = lnks[1].get_attribute('href')
+                except:
+                    review_url = 'N/A'
+                    user_url = 'N/A'
+                # get the review date and convert it to the right format
+                try:
+                    review_date = review.find('span', class_='review-date').text
+                    #review_date = review.find_element(By.CLASS_NAME, "review-date").text
+                    review_date = datetime.strptime(review_date, '%d %B %Y').strftime('%Y-%m-%d')
+                except:
+                    review_date = 'N/A'
+                # get the username
+                try:
+                    username = review.select_one('.display-name-link').text
+                except:
+                    username = 'N/A'
+                # get the comment of the user and remove the useless lines. Get only 500 characters max
+                try:
+                    comment = review.select_one('.text.show-more__control').text   #.text.replace('\n\n','\n')
+                    # comment = comment.split('\n') 
+                    # comment = [line for line in comment if not 'Was this review helpful? Sign in to vote' in line] 
+                    # comment = [line for line in comment if not 'Permalink' in line]
+                    # comment = '\n'.join(comment)
+                    #comment = comment[:500]
+                except:
+                    comment = 'N/A'
+                # store the data in a list of dictionaries
+                data_comments.append({'id': id, 'title': title, 'user': username, 'comment': comment, 'score': review_score, 'user_url': user_url, 'review_url': review_url, 'date': review_date})
+        break
+    return data_comments
 
 
 
+data_comments = get_reviews()
+
+# # write the data in a csv file
+# with open(csv_path, mode='w', newline='', encoding='utf-8') as csv_file:
+#     fieldnames = ['id', 'title', 'user', 'comment', 'score', 'user_url', 'review_url', 'date']
+#     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+#     writer.writeheader()
+#     writer.writerows(data_comments)
+
+# write the data in a json file
+with open('test_reviews.json', 'w', encoding='utf-8') as f:
+    json.dump(data_comments, f, ensure_ascii=False, indent=4)
+
+print("Data saved in Json file")
+
+# # close the driver
+# driver.quit()
+
+'''
+Set un minimum de mots (10?)
+25 commentaires par films
+Ne prendre que les films contenant contenant 25 commentaires
+'''
